de5ed73585f87dfac83c5c5df875b9fd5e5d243d
Miniezi <97309364+Miniezi@users.noreply.github.com>
2023-11-16 13:28:51 +0500

    Update and rename script.js to app.py

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-
-menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
